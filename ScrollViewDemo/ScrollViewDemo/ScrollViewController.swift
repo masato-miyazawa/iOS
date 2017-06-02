5c6902da3e3ee065b9debed132bc536845a22118
Derrick Park <park@Derricks-MacBook-Pro.local>
2017-06-02 16:39:30 -0700

    up

---
+++ ScrollViewDemo/ScrollViewDemo/ScrollViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ScrollViewController.swift
+//  ScrollViewDemo
+//
+//  Created by Derrick Park on 2017-06-02.
+//  Copyright © 2017 Derrick Park. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageView.image = UIImage(named: "image.png")
+        scrollView.backgroundColor = UIColor.black
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        automaticallyAdjustsScrollViewInsets = false
+        scrollView.contentOffset = CGPoint(x: -90, y: 450)
+        
+        setZoomScale()
+        
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        setupGestureRecognizer()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let imageViewSize = imageView.frame.size
+        let scrollViewSize = scrollView.bounds.size
+        
+        let vertivalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: vertivalPadding, left: horizontalPadding, bottom: vertivalPadding, right: horizontalPadding)
+        
+    }
+    
+    func setupGestureRecognizer() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
+        doubleTap.numberOfTapsRequired = 2
+        scrollView.addGestureRecognizer(doubleTap)
+    }
+    
+    func handleDoubleTap(recognizer: UITapGestureRecognizer) {
+        
+        if (scrollView.zoomScale > scrollView.minimumZoomScale) {
+            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+        } else {
+            scrollView.setZoomScale(scrollView.maximumZoomScale, animated: true)
+        }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        setZoomScale()
+    }
+    
+    
+    func setZoomScale() {
+        let imageViewSize = imageView.bounds.size
+        let scrollViewSize = scrollView.bounds.size
+        let widthScale = scrollViewSize.width / imageViewSize.width
+        let heightScale = scrollViewSize.height / imageViewSize.height
+        
+        scrollView.minimumZoomScale = min(widthScale, heightScale)
+        scrollView.zoomScale = 1.0
+        
+        
+    }
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+
+}
---
+++ ScrollViewDemo/ScrollViewDemo/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  ScrollViewDemo
+//
+//  Created by Derrick Park on 2017-06-02.
+//  Copyright © 2017 Derrick Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    var scrollView: UIScrollView!
+    var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView = UIImageView(image: UIImage(named: "image.png"))
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.backgroundColor = UIColor.black
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView.contentOffset = CGPoint(x: -100, y: -100)
+        scrollView.delegate = self
+        setZoomScale()
+        
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        setupGestureRecognizer()
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let imageViewSize = imageView.frame.size
+        let scrollViewSize = scrollView.bounds.size
+        
+        let vertivalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: vertivalPadding, left: horizontalPadding, bottom: vertivalPadding, right: horizontalPadding)
+        
+    }
+    
+    func setupGestureRecognizer() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
+        doubleTap.numberOfTapsRequired = 2
+        scrollView.addGestureRecognizer(doubleTap)
+    }
+    
+    func handleDoubleTap(recognizer: UITapGestureRecognizer) {
+        
+        if (scrollView.zoomScale > scrollView.minimumZoomScale) {
+            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+        } else {
+            scrollView.setZoomScale(scrollView.maximumZoomScale, animated: true)
+        }
+    }
+
+    
+    override func viewWillLayoutSubviews() {
+        setZoomScale()
+    }
+    
+    
+    func setZoomScale() {
+        let imageViewSize = imageView.bounds.size
+        let scrollViewSize = scrollView.bounds.size
+        let widthScale = scrollViewSize.width / imageViewSize.width
+        let heightScale = scrollViewSize.height / imageViewSize.height
+        
+        scrollView.minimumZoomScale = min(widthScale, heightScale)
+        scrollView.zoomScale = 1.0
+        
+        
+    }
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
